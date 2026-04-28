b97281aad84fe7e53e5e03900c836abf25bb2837
Aetherance <inkToAether@gmail.com>
2026-04-28 19:11:32 +0800

    feat(table/format): implement ReadBlock for sstable

--- src/table/format.cc
+++ src/table/format.cc
@@ -6,8 +6,13 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "env.h"
+#include "options.h"
+#include "port.h"
+#include "slice.h"
 #include "status.h"
 #include "util/coding.h"
+#include "util/crc32c.h"
 
 namespace db {
 
@@ -65,9 +70,98 @@ Status Footer::DecodeFrom(Slice* input) {
 
 Status ReadBlock(RandomAccessFile* file, const ReadOptions& options, const BlockHandle& handle,
                  BlockContents* result) {
-  // TODO:
-  // 1. implement env.h
-  // 2. implement this method
+  (void)file;
+  (void)options;
+  (void)handle;
+  result->data = Slice();
+  result->cachable = false;
+  result->heep_allocated = false;
+
+  size_t n = static_cast<size_t>(handle.size());
+  char* buf = new char[n + kBlockTrailerSize];
+  Slice contents;
+  Status s = file->Read(handle.offset(), n + kBlockTrailerSize, &contents, buf);
+  if (!s.Ok()) {
+    delete[] buf;
+    return s;
+  }
+  if (contents.Size() != n + kBlockTrailerSize) {
+    delete[] buf;
+    return Status::Corruption("read truncated block");
+  }
+
+  const char* data = contents.Data();
+  if (options.verify_checksums) {
+    const uint32_t crc = crc32c::Unmask(DecodeFixed32(data + n + 1));
+    const uint32_t actual = crc32c::Value(data, n + 1);
+    if (actual != crc) {
+      delete[] buf;
+      s = Status::Corruption("block crc checksum missmatch");
+      return s;
+    }
+  }
+
+  switch (data[n]) {
+    case kNoCompression: {
+      if (data != buf) {
+        // ::mmap()
+        // ::map() will return a memory which is managed by OS
+        delete[] buf;
+        result->data = Slice(data, n);
+        result->heep_allocated = false;
+        result->cachable = false;
+      } else {
+        // ::pread()
+        result->data = Slice(buf, n);
+        result->heep_allocated = true;
+        result->cachable = true;
+      }
+      break;
+    }
+
+    case kSnappyCompression: {
+      size_t ulength = 0;
+      if (!port::Snappy_GetUncompressedLength(data, n, &ulength)) {
+        delete[] buf;
+        return Status::Corruption("corrupted snappy compressed block length");
+      }
+      char* ubuf = new char[ulength];
+      if (!port::Snappy_Uncompress(data, n, ubuf)) {
+        delete[] buf;
+        delete[] ubuf;
+        return Status::Corruption("corrupted snappy compressed block contents");
+      }
+      delete[] buf;
+      result->data = Slice(ubuf, ulength);
+      result->heep_allocated = true;
+      result->cachable = true;
+      break;
+    }
+
+    case kZstdCompression: {
+      size_t ulength = 0;
+      if (!port::Zstd_GetUncompressedLength(data, n, &ulength)) {
+        delete[] buf;
+        return Status::Corruption("corrupted zstd compressed block length");
+      }
+      char* ubuf = new char[ulength];
+      if (!port::Zstd_Uncompress(data, n, ubuf)) {
+        delete[] buf;
+        delete[] ubuf;
+        return Status::Corruption("corrupted zstd compressed block contents");
+      }
+      delete[] buf;
+      result->data = Slice(ubuf, ulength);
+      result->heep_allocated = true;
+      result->cachable = true;
+      break;
+    }
+
+    default: {
+      delete[] buf;
+      return Status::Corruption("bad block type");
+    }
+  }
   return Status::OkStatus();
 }
 
